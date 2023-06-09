f62032c27c6fc62b94ef2aacac158f68ecf71c00
Lukas Strnad <strnad.lukas@proton.me>
2023-06-09 09:12:45 +0200

    Error handling, plus fixing long paths

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use std::{
-    fs::read_dir,
+    fs::{read_dir, read_to_string},
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
     path::Path,
 };
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = match TcpListener::bind("127.0.0.1:7878") {
+        Ok(listener) => listener,
+        Err(e) => panic!("Failed to open TcpListener: {e}"),
+    };
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        let stream = match stream {
+            Ok(stream) => stream,
+            Err(e) => {
+                eprintln!("Failed to receive incoming stream: {e}");
+                continue;
+            }
+        };
 
         handle_connnection(stream);
     }
@@ -23,25 +32,61 @@ fn handle_connnection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    let url_path = format!(".{}", http_request[0].split(' ').nth(1).unwrap());
+    let url_path = format!(
+        "./{}",
+        http_request[0].split(' ').nth(1).map_or_else(
+            || {
+                eprintln!("Invalid request");
+                ""
+            },
+            |request| request.trim_start_matches('/')
+        )
+    );
     let path = Path::new(url_path.as_str());
 
     let body = if path.is_dir() {
-        // TODO: Show all the files and subfolders
         let mut files_list = String::new();
 
-        let paths = read_dir(path).unwrap();
+        let paths = match read_dir(path) {
+            Ok(paths) => paths,
+            Err(e) => {
+                eprintln!("Failed to read directory: {e}");
+                return;
+            }
+        };
         for content in paths {
-            let file_name = content.unwrap().file_name();
-            let file = file_name.to_str().unwrap();
+            let file_name = match content {
+                Ok(file) => file.file_name(),
+                Err(e) => {
+                    eprintln!("Failed to read file inside this directory: {e}");
+                    continue;
+                }
+            };
+            let file = file_name.to_str().unwrap_or("");
 
-            files_list = format!("{files_list}<li><a href=\"{}/{file}\">{file}</a></li>", path.as_os_str().to_str().unwrap());
+            files_list = format!(
+                "{files_list}<li><a href=\"/{}/{file}\">{file}</a></li>",
+                path.as_os_str()
+                    .to_str()
+                    .unwrap_or("")
+                    .trim_start_matches('.')
+                    .trim_start_matches('/')
+            );
         }
 
         format!("<h1>File Server</h1><ul>{files_list}</ul>")
     } else {
-        // TODO: Show the file
-        std::fs::read_to_string(path).unwrap()
+        format!(
+            r#"<textarea style="width: 100%; height: 100%;">{}</textarea>"#,
+            match read_to_string(path) {
+                Ok(content) => content,
+                Err(e) => {
+                    let msg = format!("Failed to read file: {e}");
+                    eprintln!("{msg}");
+                    msg
+                }
+            }
+        )
     };
 
     let status_line = "HTTP/1.1 200 OK";
@@ -60,5 +105,8 @@ fn handle_connnection(mut stream: TcpStream) {
 </html>"
     );
 
-    stream.write_all(response.as_bytes()).unwrap();
+    match stream.write_all(response.as_bytes()) {
+        Ok(_) => {}
+        Err(e) => eprintln!("Failed to write response: {e}"),
+    };
 }
